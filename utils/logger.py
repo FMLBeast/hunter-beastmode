9e7ccad14709620f235e1c8f222dc120bad753c9
beast <fmlbeastmode@gmail.com>
2025-06-20 16:34:41 +0200

    big fixing round

--- utils/logger.py
+++ utils/logger.py
@@ -3,6 +3,6 @@ Logger utility stub.
 """
 import logging
 
-def setup_logging():
-    logging.basicConfig(level=logging.INFO)
+def setup_logging(level=logging.INFO, **kwargs):
+    logging.basicConfig(level=level, **kwargs)
     return logging.getLogger("steg")
